76906e7aad9d442b3dfa45772b7655b2c2c43aaa
vifird <vifird@163.com>
2016-12-01 21:31:56 +0800

    Alter.

--- example/main/pages/products/LeftNav.js
+++ example/main/pages/products/LeftNav.js
@@ -9,16 +9,16 @@ export default class LeftNav extends React.Component {
       <div className="col-md-2">
         <ul className="nav nav-pills nav-stacked">
           <li className={ Control.path === '/products/ep'? 'active' : '' }>
-            <Link href="/products/ep">Enterprise Website</Link>
+            <Link to="/products/ep">Enterprise Website</Link>
           </li>
           <li className={ Control.path === '/products/mo' || Control.path === '/products'? 'active' : '' }>
-            <Link href="/products/mo">Mobile Website</Link>
+            <Link to="/products/mo">Mobile Website</Link>
           </li>
           <li className={ Control.path === '/products/item/first'? 'active' : '' }>
-            <Link href="/products/item/first">Detail First</Link>
+            <Link to="/products/item/first">Detail First</Link>
           </li>
           <li className={ Control.path === '/products/item/second'? 'active' : '' }>
-            <Link href="/products/item/second">Detail Second</Link>
+            <Link to="/products/item/second">Detail Second</Link>
           </li>
         </ul>
       </div>
